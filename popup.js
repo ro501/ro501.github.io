b471ab10379f14c03ff364587858bb235e4852ec
root <root@ro706>
2023-09-03 03:10:49 +0530

    happy birthday shreya

---
+++ popup.js
@@ -0,0 +1,12 @@
+
+
+
+window.addEventListener("load",
+
+function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").computedStyleMap.display="block"
+        },2000
+    )
+})
